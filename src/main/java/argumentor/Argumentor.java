d06bf65126d6debfd08c1e5d750187b06742e1bb
Fürste <joshua.fuerste@rwth-aachen.de>
2020-07-13 16:41:32 +0200

    Initial commit

---
+++ src/main/java/argumentor/Argumentor.java
@@ -0,0 +1,44 @@
+package argumentor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Argumentor {
+
+    Map<String, Boolean> booleanFlags = new HashMap<>();
+
+    void addBooleanArgument(String id, String... alternativeNames) {
+
+        booleanFlags.put(id, false);
+    }
+
+    public void parseArguments(String[] args) {
+        for (String arg : args) {
+            if (isMultipleBooleanFlagsArg(arg)) {
+                setMultipleFlags(arg);
+            } else if (arg.charAt(0) == '-' && arg.length() == 2){
+                setSingleFlag(arg);
+            }
+        }
+    }
+
+    private void setSingleFlag(String arg) {
+        
+    }
+
+    private boolean isMultipleBooleanFlagsArg(String arg) {
+        return (arg.charAt(0) == '-') && (arg.length() > 2);
+    }
+
+    private void setMultipleFlags(String arg) {
+        String[] splitFlags = arg.substring(1).split("(?!^)");
+        for (String flag : splitFlags) {
+            booleanFlags.put(flag, true);
+        }
+    }
+
+
+    public boolean getBooleanArgument(String c) {
+        return booleanFlags.get(c);
+    }
+}
---
+++ src/test/java/argumentor/ArgumentorTest.java
@@ -0,0 +1,38 @@
+package argumentor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArgumentorTest {
+
+    @Test
+    public void emptyBooleanTest(){
+        Argumentor argumentor = getOneFlagArgumentor();
+        argumentor.parseArguments(new String[1]);
+        boolean cArg = argumentor.getBooleanArgument("c");
+        assertFalse(cArg);
+    }
+
+    private Argumentor getOneFlagArgumentor() {
+        Argumentor argumentor = new Argumentor();
+        argumentor.addBooleanArgument("c");
+        return argumentor;
+    }
+
+    @Test
+    public void singleFlagTest(){
+        Argumentor argumentor = getOneFlagArgumentor();
+        argumentor.parseArguments(getSimpleBooleanArg());
+        boolean cArg = argumentor.getBooleanArgument("c");
+        assertTrue(cArg);
+    }
+
+    private String[] getSimpleBooleanArg() {
+        String[] args = new String[1];
+        args[0] = "-cb";
+        return args;
+    }
+
+
+}
